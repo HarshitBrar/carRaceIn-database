18d5faa7618736828801a1dc387da97acefca8ae
HarshitBrar <harshitbrar.1305@gmail.com>
2021-01-13 20:02:47 +0530

    hello

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,7 @@
 class Game {
-  constructor(){}
+  constructor(){
+
+  }
   
   getState(){
     var gameStateRef  = database.ref('gameState');
@@ -15,12 +17,41 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        form = new Form()
+      }
       player.getCount();
-      form = new Form()
       form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(24)
+    text("game has started",120,100);
+    Player.getPlayerInfo()
+    if(allPlayers !== undefined){
+      var position = 130;
+      for(var plr in allPlayers){
+        if(plr === "player"+player.index){
+          fill("red");
+        }
+        else{
+          fill("black");
+        }
+        position = position + 20
+        textSize(15);
+        text(allPlayers[plr].name +":"+ allPlayers[plr].distance,120,position);
+        }
+    }
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance = player.distance + 50
+      player.update();
     }
   }
 }
